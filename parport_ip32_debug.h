56f9a09751f0b7bfeaa3fad0efd6fea31d040842
arnaud <arnaud>
2007-03-17 10:25:03 +0000

    copyright info

--- parport_ip32_debug.h
+++ parport_ip32_debug.h
@@ -1,3 +1,22 @@
+/* Debugging routines for parport_ip32
+ *
+ * Copyright (C) 2005-2007 Arnaud Giersch.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 /* DEBUG_PARPORT_IP32
  *	0	disable debug
  *	1	standard level: pr_debug1 is enabled
